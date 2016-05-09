cb575cab59818f6b7aeb6de4f4dedc325f2ccf61
deanturpin <git@lvbtn.uk>
2016-05-09 18:28:23 +0100

    remove objects

--- sandbox/11/regular_expressions.cpp
+++ sandbox/11/regular_expressions.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <regex>
 
-void regular_expressions()
-{
+void regular_expressions() {
+
+	using namespace std;
+
+	string search("blah yes blah no");
+	regex expression("blah");
+
+	if (regex_match(search, expression))
+		cout << "Match yes" << endl;
+	else
+		cout << "Match no" << endl;
 }
