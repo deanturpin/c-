0ea0e42379a598458e1e373ed26ed8ffaee69123
deanturpin <git@lvbtn.uk>
2016-05-09 22:22:04 +0100

    add basic containers, see issue #2

---
+++ sandbox/03/containers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+
+void containers()
+{
+	using namespace std;
+
+	// Basic containers
+	vector<int> v; // Contiguous
+	deque<int> d;
+	list<int> l;
+
+	// Adapters
+}
