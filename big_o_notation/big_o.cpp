7d5d55a40f83cb1a820f04107171460fa6f67990
deanturpin <git@lvbtn.uk>
2016-06-01 22:55:05 +0100

    headings

--- big_o_notation/big_o.cpp
+++ big_o_notation/big_o.cpp
@@ -5,5 +5,11 @@ int main() {
 	using namespace std;
 	cout << "Big O notation" << endl;
 
+	cout << "Same time\tO(1)" << endl;
+	cout << "Linear\t\tO(N)" << endl;
+	cout << "Square\t\tO(N^2)" << endl;
+	cout << "Exponential\tO(2^N)" << endl;
+	cout << "Logarithm\tO(log N)" << endl;
+
 	return 0;
 }
