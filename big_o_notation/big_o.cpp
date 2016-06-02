aa56a4a5b63641f31efab2322e8f42fd79355927
deanturpin <git@lvbtn.uk>
2016-06-02 09:03:06 +0100

    creating heading function and separate constant out

--- big_o_notation/big_o.cpp
+++ big_o_notation/big_o.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
+#include <vector>
+
+namespace bigo
+{
+	using namespace std;
+
+	// Common heading
+	void heading(const std::string &name)
+	{
+		cout << name << endl;
+		cout << "----------" << endl;
+	}
+
+	// Constant 
+	void constant()
+	{
+		heading("Constant\tO(1)");
+		cout << "Vector index" << endl;
+		vector<int> vec(10000, 0);
+		for (auto &i: vec)
+			++i;
+	}
+}
 
 int main() {
 
 	using namespace std;
-	cout << "Big O notation" << endl;
 
-	cout << "Same time\tO(1)" << endl;
-	cout << "Linear\t\tO(N)" << endl;
-	cout << "Square\t\tO(N^2)" << endl;
-	cout << "Exponential\tO(2^N)" << endl;
-	cout << "Logarithm\tO(log N)" << endl;
+	bigo::constant();
+	bigo::heading("Linear\t\tO(N)");
+	bigo::heading("Square\t\tO(N^2)");
+	bigo::heading("Exponential\tO(2^N)");
+	bigo::heading("Logarithm\tO(log N)");
 
 	return 0;
 }
