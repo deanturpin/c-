b81f82488ab1110436346dad6a7c8455bc2819ad
deanturpin <git@lvbtn.uk>
2016-06-05 19:42:30 +0100

    bitwise

---
+++ sandbox/03/bitwise_operators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void bitwise_operators()
+{
+	using namespace std;
+
+	const unsigned int left = 0x0101;
+	const unsigned int right = 0x1010;
+
+	// Unmodified
+	cout << left << endl;
+	cout << right << endl;
+
+	// Bitwise not
+	cout << ~left << endl;
+
+	// And, or, xor
+	cout << (left & right) << endl;
+	cout << (left | right) << endl;
+	cout << (left ^ right) << endl;
+
+	// Shift 
+	cout << (left << 1) << endl;
+	cout << (left >> 1) << endl;
+}
