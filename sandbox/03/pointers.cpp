e86f628a6de97c5f149392e23a350e07e4b96e17
deanturpin <git@lvbtn.uk>
2016-06-05 19:32:53 +0100

    basic pointers meaning of const * const

---
+++ sandbox/03/pointers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void pointers() {
+
+	int foo = 5;
+	int bah = 5;
+
+	// const pointer
+	int       * const p = &bah;
+	*p = 6;
+	// Error - can't change the destination
+	// p = &foo;
+
+	// const type
+	int const *       q = &bah;
+	q = &foo;
+	// Error - can't change the value
+	// *q = 6;
+
+	std::cout << "foo " << foo << " bah " << bah << std::endl;
+	std::cout << "p " << *p << " q " << *q << std::endl;
+}
