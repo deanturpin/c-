abb59d21ee801c6b4290bd7963be1f07399d2bbe
deanturpin <git@lvbtn.uk>
2016-06-11 19:19:47 +0100

    elseif shortcut

---
+++ standard/03/elseif.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void elseif()
+{
+	int i = -1;
+
+	int b =
+		i == 1 ? 1 :
+		i == 2 ? 2 :
+		i == 3 ? 3 :
+		i == 4 ? 4 : 5;
+
+	std::cout << "b is " << b << std::endl;
+
+	// Is comparable to 
+
+	int c = 0;
+
+	if (i == 1)
+		c = 1;
+	else if (i == 2)
+		c = 2;
+	else if (i == 3)
+		c = 3;
+	else if (i == 4)
+		c = 4;
+	else
+		c = 5;
+
+	std::cout << "c is " << b << std::endl;
+}
