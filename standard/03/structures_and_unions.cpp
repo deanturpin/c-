dd3d170e365f1617b3aac241554564cdc147d929
deanturpin <git@lvbtn.uk>
2016-06-11 15:57:06 +0100

    add op ==

--- standard/03/structures_and_unions.cpp
+++ standard/03/structures_and_unions.cpp
@@ -2,17 +2,26 @@
 
 void structures_and_unions()
 {
+	using namespace std;
+
 	union A
 	{
 		int foo;
 		char bar[4];
+
+		bool operator==(const A &rhs) const { return foo == rhs.foo; }
 	};
 
-	union A a;
+	A a;
 	a.bar[0] = 0xaa;
 	a.bar[1] = 0x55;
 	a.bar[2] = 0xaa;
 	a.bar[3] = 0x55;
 
-	std::cout << std::hex << a.foo << std::endl;
+	A b;
+
+	cout << hex << a.foo << endl;
+	cout << "Address of a - " << hex << reinterpret_cast<unsigned int>(&a) << endl;
+
+	cout << "Does b equal a? " << boolalpha << (b == a) << endl;
 }
