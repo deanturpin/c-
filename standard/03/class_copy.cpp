da328dfb4bdb5a72aeae0283cdf71ecbaab4cf7f
deanturpin <git@lvbtn.uk>
2016-07-26 13:17:30 +0100

    class copy

---
+++ standard/03/class_copy.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+namespace mus {
+	using namespace std;
+
+	class instrument {
+
+		public:
+			int numberOfStrings;
+			bool electric;
+			string* name;
+
+			instrument() {
+				name = new string("instrument");
+			}
+
+			instrument(const class instrument &i) {
+				name = new string(*i.name + " copied");
+				numberOfStrings = i.numberOfStrings;
+			}
+
+			~instrument() {
+				delete name;
+			}
+	};
+}
+
+int main() {
+
+	using namespace std;
+	using namespace mus;
+
+	// Create some instruments
+	instrument a;
+	a.numberOfStrings = 5;
+
+	instrument b(a);
+
+	cout << "a strings " << a.numberOfStrings << endl;
+	cout << "a name " << *a.name << endl;
+	cout << "b strings " << b.numberOfStrings << endl;
+	cout << "b name " << *b.name << endl;
+
+	return 0;
+}
