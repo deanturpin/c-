d67de2c5f0825bf7e393ddf7880e2380f532a5bd
deanturpin <git@lvbtn.uk>
2016-09-06 12:51:53 +0100

    read a random int from /dev/urandom

--- random_noise/random_noise.cpp
+++ random_noise/random_noise.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
 
 	using namespace std;
 
 	cout << "Random noise" << endl;
+
+	const string randomFile = "/dev/urandom";
+	cout << "Using " << randomFile << endl;
+
+	// stringstream oui;
+	int blah;
+	auto is = ifstream(randomFile);
+	is.read(reinterpret_cast<char *>(&blah), sizeof blah);
+
+	cout << "Read " << blah << endl;
 
 	return 0;
 }
