9959c8a7f419f347b306b25b340e9727ed8134d2
deanturpin <git@lvbtn.uk>
2016-09-10 15:50:22 +0100

    first go at recursion

--- flood_fill/flood_fill.cpp
+++ flood_fill/flood_fill.cpp
@@ -6,8 +6,7 @@ namespace flood {
 	using namespace std;
 
 	std::vector<std::string> bitmap = {
-		"x                               ",
-		"        xxxxxxxxxxxxxxx         ",
+		"x       xxxxxxxxxxxxxxx         ",
 		"        x              x        ",
 		"        x              x        ",
 		"        x              x        ",
@@ -19,31 +18,35 @@ namespace flood {
 		"                   xxxx         ",
 		"                                ",
 		"                                ",
-		"                                "
+		"                                ",
+		"                                ",
+		"                   xxxx         ",
 	};
 
 	void dump() {
 	
 		// Top and bottom border
-		const string border = string(bitmap.front().size() + 2, '_');
+		const string border = "\t" + string(bitmap.front().size() + 1, '_');
 		cout << border << endl;
 
-		for (const auto &raster : bitmap) {
+		const unsigned int width = bitmap.size();
+		const unsigned int height = bitmap.front().size();
 
-			cout << "|";
+		for (unsigned int w = 0; w < width; ++w) {
+			cout << w << "\t|";
 
-			for (const auto &c : raster)
-				cout << c;
+			for (unsigned int h = 0; h < height; ++h)
+				cout << bitmap[w][h];
 
 			cout << "|" << endl;
 		}
-
+				
 		cout << border << endl;
 	}
 
 	bool fill(const unsigned int x, const unsigned int y) {
 
-		cout << "Start point " << x << ", " << y << endl;
+		// cout << "Start point " << x << ", " << y << endl;
 
 		// Check range
 		if (x > bitmap.front().size() || y > bitmap.size()) {
@@ -53,8 +56,18 @@ namespace flood {
 		}
 
 		// Check start point
-		cout << (bitmap[x][y] == 'x' ? "Bad " : "Good") << " start" << endl;
-		cout << '\"' << bitmap[x][y] << '\"' << endl;
+		if (bitmap[x][y] != 'x') {
+
+			bitmap[x][y] = 'x';
+
+			fill(x + 1, y + 1);
+			fill(x + 1, y);
+			fill(x + 1, y - 1);
+
+			fill(x - 1, y + 1);
+			fill(x - 1, y);
+			fill(x - 1, y - 1);
+		}
 
 		return true;
 	}
@@ -66,7 +79,7 @@ int main() {
 	flood::dump();
 
 	// Print if flood fill is successful
-	if (flood::fill(8, 0))
+	if (flood::fill(2, 12))
 		flood::dump();
 	
 	return 0;
