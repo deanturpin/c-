7f8bfaf4693e0c36e21f67453065f32649b3f7d9
deanturpin <git@lvbtn.uk>
2016-09-10 10:57:41 +0100

    check ranges

--- flood_fill/flood_fill.cpp
+++ flood_fill/flood_fill.cpp
@@ -22,14 +22,31 @@ namespace flood {
 		"                                "
 	};
 
-	void fill(const int x, const int y) {
+	void dump() {
+		
+		for (const auto &raster : bitmap)
+			cout << raster << endl;
+	}
+
+	void fill(const unsigned int x, const unsigned int y) {
+
 		cout << "Start at " << x << ", " << y << endl;
+
+		// Check range
+		if (x > bitmap.front().size() || y > bitmap.size()) {
+			
+			cout << "Start point out of range" << endl;
+			return;
+		}
 	}
 }
 
 int main() {
 
+	flood::dump();
 	flood::fill(6, 7);
+	flood::fill(100, 1000);
+	flood::dump();
 	
 	return 0;
 }
