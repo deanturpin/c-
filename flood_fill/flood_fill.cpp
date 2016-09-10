bf3b1556a1fc54afdaff40635b0ff3b367e9b356
deanturpin <git@lvbtn.uk>
2016-09-10 11:08:45 +0100

    check start point

--- flood_fill/flood_fill.cpp
+++ flood_fill/flood_fill.cpp
@@ -6,6 +6,7 @@ namespace flood {
 	using namespace std;
 
 	std::vector<std::string> bitmap = {
+
 		"                                ",
 		"        xxxxxxxxxxxxxxx         ",
 		"        x              x        ",
@@ -28,7 +29,7 @@ namespace flood {
 			cout << raster << endl;
 	}
 
-	void fill(const unsigned int x, const unsigned int y) {
+	bool fill(const unsigned int x, const unsigned int y) {
 
 		cout << "Start at " << x << ", " << y << endl;
 
@@ -36,16 +37,24 @@ namespace flood {
 		if (x > bitmap.front().size() || y > bitmap.size()) {
 			
 			cout << "Start point out of range" << endl;
-			return;
+			return false; 
 		}
+
+		// Check start point
+		if (bitmap[x][y] == 'x')
+			cout << "Bad start point" << endl;
+		else
+			cout << "Good start point" << endl;
+
+		return true;
 	}
 }
 
 int main() {
 
 	flood::dump();
-	flood::fill(6, 7);
-	flood::fill(100, 1000);
+	flood::fill(10, 10);
+	flood::fill(8, 1);
 	flood::dump();
 	
 	return 0;
