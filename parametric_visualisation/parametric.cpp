19dc814413df52667bbd80ab1747c6e2a0b8cf40
deanturpin <git@lvbtn.uk>
2016-10-04 12:55:21 +0100

    make canvas larger, add parametric data

--- parametric_visualisation/parametric.cpp
+++ parametric_visualisation/parametric.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 
+
 int main() {
 
 	using namespace std;
 
-	const unsigned int X = 50;
-	const unsigned int Y = 20;
+	const unsigned int X = 75;
+	const unsigned int Y = 40;
 
 	// Create an empty raster
 	vector<bool> raster(X, false);
@@ -14,25 +15,38 @@ int main() {
 	// Initialise bitmap
 	vector<vector<bool>> bitmap(Y, raster);
 
-	// Scribble
-	for (int i = 0; i < 10; ++i)
-		bitmap[i][i] = 1;
+	// Some params
+	struct P {
 
-	const string rule(X + 2, '-');
-	cout << rule << endl;
+		unsigned int x;
+		unsigned int y;
+		unsigned int z;
+	};
+
+	vector<P> params = {
+		{0, 1, 2},
+		{0, 2, 2},
+		{0, 3, 2},
+		{0, 4, 3},
+		{2, 1, 3},
+		{2, 1, 3},
+		{2, 1, 3},
+	};
+
+	// Populate bitmap
+	for (const auto &p : params)
+		bitmap[p.x][p.y] = true;
 
 	// Print bitmap
+
 	for (const auto &y : bitmap) {
 
-		cout << '|';
-
-		for (const auto &x : y)
-			cout << (0 == x ? ' ' : '0');
+		for (const auto x : y)
+			cout << (0 == x ? ' ' : '.');
 
 		cout << '|' << endl;
 	}
-
-	cout << rule << endl;
+	cout << string(X, '_') << '|'<< endl;
 
 	return 0;
 }
