2b4652d2c52c9cdd70501214c0d35bfd8ef97bcb
deanturpin <git@lvbtn.uk>
2016-10-04 08:49:15 +0100

    init bitmap

---
+++ random_bitmap/random_bitmap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+/*
+std::vector<std::vector<bool>> bitmap = {
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+};
+*/
+
+int main() {
+
+	using namespace std;
+
+	// Print bitmap
+	for (const auto &y : bitmap) {
+
+		for (const auto &x : y)
+			cout << x;
+
+		cout << endl;
+	}
+
+	return 0;
+}
