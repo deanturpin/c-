a8fdbed4b10931a431337f61d4cf6e7ceddd11cf
deanturpin <git@lvbtn.uk>
2016-10-07 15:26:17 +0100

    put sequence in main to avoid global destructor warning

--- dev/sorting-visualisation/sort-vis.cpp
+++ dev/sorting-visualisation/sort-vis.cpp
@@ -1,56 +1,23 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-
-extern std::vector<int> sequence;
 
 int main() {
 
 	using namespace std;
 
-	cout << "Length of sequence " << sequence.size() << endl;
+	// A sequence to sort
+	std::vector<unsigned int> sequence = {
+		13, 6, 30, 17, 6, 28, 25, 22, 13, 24, 29, 9, 6, 21, 12, 16, 11, 16, 11, 12,
+		1, 31, 3, 15, 12, 10, 18, 11, 13, 28, 10, 28, 21, 5, 27, 22, 12, 26, 24, 8
+	};
+
+	cout << "Sequence length " << sequence.size() << endl;
+
+	// Print sequence
+	for_each (sequence.cbegin(), sequence.cend(), [](auto &n)
+		{ cout << string(n, '-') << endl; });
 
 	return 0;
 }
 
-std::vector<int> sequence = {
-	13,
-	6,
-	30,
-	17,
-	6,
-	28,
-	25,
-	22,
-	13,
-	24,
-	29,
-	9,
-	6,
-	21,
-	12,
-	16,
-	11,
-	16,
-	11,
-	12,
-	1,
-	31,
-	3,
-	15,
-	12,
-	10,
-	18,
-	11,
-	13,
-	28,
-	10,
-	28,
-	21,
-	5,
-	27,
-	22,
-	12,
-	26,
-	24,
-	8
-};
