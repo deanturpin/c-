570a818df7b8797fb9aa6c939ea19788d4575b43
deanturpin <git@lvbtn.uk>
2016-10-07 08:17:28 +0100

    add random sequence

---
+++ dev/sorting-visualisation/sort-vis.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+extern std::vector<int> sequence;
+
+int main() {
+
+	using namespace std;
+
+	cout << "Length of sequence " << sequence.size() << endl;
+
+	return 0;
+}
+
+std::vector<int> sequence = {
+	13,
+	6,
+	30,
+	17,
+	6,
+	28,
+	25,
+	22,
+	13,
+	24,
+	29,
+	9,
+	6,
+	21,
+	12,
+	16,
+	11,
+	16,
+	11,
+	12,
+	1,
+	31,
+	3,
+	15,
+	12,
+	10,
+	18,
+	11,
+	13,
+	28,
+	10,
+	28,
+	21,
+	5,
+	27,
+	22,
+	12,
+	26,
+	24,
+	8
+};
