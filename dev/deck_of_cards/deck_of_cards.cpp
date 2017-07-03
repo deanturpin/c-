c2d75a06148073de09412699d968d8f2666e1827
deanturpin <git@lvbtn.uk>
2017-07-03 13:17:45 +0100

    pc

--- dev/deck_of_cards/deck_of_cards.cpp
+++ dev/deck_of_cards/deck_of_cards.cpp
@@ -14,20 +14,20 @@ namespace deck {
 
 		public:
 
-			// Initialise deck
-			cards() {
+		// Initialise deck
+		cards() {
 
-				for (const auto &f : face)
-					for (const auto &s : suit)
-						c.push_back(make_pair(f, s));
-			}
+			for (const auto &f : face)
+				for (const auto &s : suit)
+					c.push_back(make_pair(f, s));
+		}
 
-			// Print each card
-			void dump() {
-				
-				for (const auto &card : c)
-					cout << card.first << " " << card.second << endl;
-			}
+		// Print each card
+		void dump() {
+
+			for (const auto &card : c)
+				cout << card.first << " " << card.second << endl;
+		}
 
 	};
 }
