dfec3e0c513e1236ab1e6b54b14109508ee53f76
deanturpin <git@lvbtn.uk>
2017-09-06 18:11:00 +0100

    Add count_if

---
+++ algorithm.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main() {
+
+  using namespace std;
+
+  const vector<long> vec = { 1, 2, 3, 4, 5, 6 };
+
+  const auto count = count_if(vec.cbegin(), vec.cend(),
+           [](const auto &a){ return a < 3; });
+
+  cout << count << endl;
+
+  return 0;
+}
