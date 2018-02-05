9d6fcc8967a710d7f79b3c070cd49908ba4a7612
deanturpin <git@lvbtn.uk>
2018-02-05 06:48:03 +0000

    First implementation of thread pool

--- threads.cpp
+++ threads.cpp
@@ -19,39 +19,80 @@ void process_file(const std::string name) {
 
 int main() {
 
-  std::queue<std::string> files({
-    "file1",
-    "file2",
-    "file3",
-    "file4",
-    "file5",
-    "file6",
+  const unsigned deep_end = std::thread::hardware_concurrency();
+  std::cout << deep_end << " threads available\n";
+
+  std::vector<std::string> files({
+    "file0.txt",
+    "file1.txt",
+    "file2.txt",
+    "file3.txt",
+    "file4.txt",
+    "file5.txt",
+    "file6.txt",
+    "file7.txt",
   });
 
   std::vector<std::thread> threads;
 
-  while (!files.empty()) {
-    const auto file = files.front();
-    threads.push_back(std::thread(process_file, file));
-    files.pop();
+  for (auto f : files) {
+    for (auto i = 0UL; i < deep_end; ++i) {
 
-    std::cout << threads.size() << " threads\n";
+      if (files.empty())
+        break;
 
-    // If we've created enough threads wait for them
-    // if (threads.size() == 1)
-    //   for (auto &t : threads) {
-    //     std::cout << *reinterpret_cast<unsigned int *>(&t) << " join\n";
-    //     t.join();
-    // }
+      const auto file = files.back();
 
-    // And then clear down the threads
+      threads.push_back(
+        std::thread([](const auto &f){ std::cout << f << " lambda\n"; }, file)
+        );
+
+      threads.back().join();
+
+      files.pop_back();
+    }
+
+    std::cout << "Clear down the threads\n";
+
+    // std::cout << "Clear down the threads\n";
     // threads.clear();
   }
 
-  for (auto &t : threads) {
-    std::cout << *reinterpret_cast<unsigned int *>(&t) << " join\n";
-    t.join();
+  std::cout << threads.size() << " threads created\n";
+
+  /*
+  for (const auto &file : files) {
+
+    // const auto file = files.back();
+    // threads.push_back(std::thread(process_file, file));
+    threads.push_back(
+      std::thread([](const auto &f){ std::cout << f << " lambda\n"; }, file)
+      );
+    // files.pop_back();
+
+    // std::cout << threads.size() << " threads\n";
+
+    // If we've created enough threads wait for them
+    if (threads.size() == 1)
+      for (auto &t : threads) {
+        std::cout << file << " join\n";
+        t.join();
+        std::cout << file << " joined\n";
+    }
+
+    std::cout << "All joined up\n";
+
+    // And then clear down the threads
+    threads.clear();
   }
+
+  std::cout << threads.size() << " threads created\n";
+
+  // for (auto &t : threads) {
+  //   std::cout << *reinterpret_cast<unsigned int *>(&t) << " join\n";
+  //   t.join();
+  // }
+  */
 
   return 0;
 }
