832afa6827624f17f33e622545d4bae7bbc96a01
deanturpin <git@lvbtn.uk>
2018-02-22 12:44:42 +0000

    Use vector of strategies

--- trade.cpp
+++ trade.cpp
@@ -3,66 +3,59 @@
 #include <numeric>
 #include <algorithm>
 #include <string>
+#include <memory>
 #include <vector>
 
 struct turbo {
 
   const std::string name = "turbo_20";
+  const double threshold = 1.1;
 
   turbo() { std::cout << "turbo()\n"; }
   turbo(const turbo &c) { std::cout << "turbo(const turbo&)\n"; }
-  turbo(const turbo &&) { std::cout << "turbo(turbo&&)\n"; }
-  turbo &operator=(const turbo &) = delete;
-  turbo &operator=(turbo &&) = delete;
+  turbo(const turbo &&) = delete;
+  turbo &operator=(const turbo &) = default;
+  turbo &operator=(turbo &&) = default;
   virtual ~turbo() { std::cout << "~turbo()\n"; }
 
-  virtual void buy() const { std::cout << "turbo buy\n"; }
+  virtual void buy() const { std::cout << "turbo buy " << threshold << "\n"; }
   virtual void sell() const { std::cout << "turbo sell\n"; }
 };
 
 struct turbonew : public turbo {
 
   const std::string name = "turbonew";
+  const double threshold = 2.1;
 
   turbonew() { std::cout << "turbonew()\n"; }
   turbonew(const turbonew &c) { std::cout << "turbonew(const turbonew&)\n"; }
-  turbonew(const turbonew &&) { std::cout << "turbonew(turbonew&&)\n"; }
+  turbonew(const turbonew &&) = delete;
   turbonew &operator=(const turbonew &) = delete;
-  turbonew &operator=(turbonew &&) = delete;
+  turbonew &operator=(turbonew &&) = default;
   virtual ~turbonew() { std::cout << "~turbonewnew()\n"; }
 
-  void buy() const override { std::cout << "turbonew buy\n"; }
+  void buy() const override { std::cout << "turbonew buy << " << threshold << "\n"; }
 };
 
 int main() {
 
+  std::cout << "\n## not vector\n";
   {
-    // not vector
-    std::cout << "\n## not vector\n";
     turbo a;
     turbonew b;
 
     a.buy();
-    a.sell();
     b.buy();
-    b.sell();
+  }
 
-    // vector
-    std::cout << "\n## vector\n";
-    // std::vector<turbo> s {
-    //   turbo(),
-    //   turbonew()
-    // };
+  std::cout << "\n## vector dynamic\n";
+  {
+    std::vector<std::shared_ptr<turbo>> s {
+         std::move(std::shared_ptr<turbo>(new turbo())),
+         std::move(std::shared_ptr<turbo>(new turbonew())),
+    };
 
-    std::vector<turbo> s;
-    s.push_back(a);
-    s.push_back(b);
-
-    for (const auto &i : s) {
-      i.buy();
-      i.sell();
-    }
-
-    std::cout << "\n## clear\n";
+    for (const auto &i : s)
+      i->buy();
   }
 }
