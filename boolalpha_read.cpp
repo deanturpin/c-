0f1c4045e00b68a9a6fce6ac0829096c459e3dfa
deanturpin <git@lvbtn.uk>
2018-02-26 12:17:26 +0000

    Don't need to set precision on way in

--- boolalpha_read.cpp
+++ boolalpha_read.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 
@@ -10,21 +11,28 @@ int main() {
   const std::string name("/tmp/blah.txt");
 
   std::string one = "one";
-  double two = 2;
+  double two = 1.123456789;
   bool three = false;
   bool four = true;
 
+  cout << "OUT\t" << std::setprecision(10) << one << " "
+    << two << " "
+    << std::boolalpha << three << " "
+    << four << "\n";
+
   ofstream out(name);
+  out.precision(10);
   out << one << " "
     << two << " "
     << std::boolalpha << three << " "
     << four << "\n";
   out.close();
 
+  // Don't need to set precision on way in
   ifstream in(name);
   in >> one >> two >> three >> four;
 
-  cout << one << " "
+  cout << "IN\t" << std::setprecision(10) << one << " "
     << two << " "
     << std::boolalpha << three << " "
     << four << "\n";
