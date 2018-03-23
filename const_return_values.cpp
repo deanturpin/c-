2d1db17a73e4b40cec6eb43423f12dff572940cc
deanturpin <git@lvbtn.uk>
2018-03-23 14:02:27 +0000

    test const return values

---
+++ const_return_values.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+const bool return_builtin() {
+  return true;
+}
+
+struct rah {};
+
+const rah return_userdefined() {
+  return rah();
+}
+
+int main() {
+
+  auto b = return_builtin();
+  b = false;
+
+  auto r = return_userdefined();
+  r = rah();
+
+  return b;
+}
