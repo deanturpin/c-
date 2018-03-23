aa010c193028998377557eb0382fe9e5792799ca
DͮEͦAͭNͤ <github@lvbtn.uk>
2018-03-23 08:42:35 +0000

    remove weird comment

--- consts_in_for_loops.cpp
+++ consts_in_for_loops.cpp
@@ -1,12 +1,10 @@
 #include <vector>
 
-// Type your code here, or load an example.
 int main() {
 
   const long blah = 5;
   std::vector<long> vec = {1, 2, 3, 4, 5};
 
-  for (auto &i : vec) {
+  for (auto &i : vec)
     i += blah;
-  }
 }
