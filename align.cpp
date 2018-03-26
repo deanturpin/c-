0b3b0ad208f3740f992044bb3afd89470d0b3d29
deanturpin <git@lvbtn.uk>
2018-03-26 08:14:22 +0100

    Print the things we're aligning

--- align.cpp
+++ align.cpp
@@ -2,19 +2,21 @@
 
 int main() {
 
-  using namespace std;
+  using std::cout;
+  using std::alignment_of;
 
   class A {};
   class alignas(32) B {};
 
-  cout << alignment_of<A>::value << endl;
-  cout << alignment_of<B>::value << endl;
-  cout << alignment_of<int>::value << endl;
-  cout << alignment_of<double>::value << endl;
-  cout << alignof(A) << endl;
-  cout << alignof(B) << endl;
-  cout << alignof(int) << endl;
-  cout << alignof(double) << endl;
+  cout << "class A {};\n";
+  cout << "class alignas(32) B {};\n\n";
 
-  return 0;
+  cout << alignment_of<A>::value << "\talignment_of<A>::value\n";
+  cout << alignment_of<B>::value << "\talignment_of<B>::value\n";
+  cout << alignment_of<int>::value << "\talignment_of<int>::value\n";
+  cout << alignment_of<double>::value << "\talignment_of<double>::value\n";
+  cout << alignof(A) << "\talignof(A)\n";
+  cout << alignof(B) << "\talignof(B)\n";
+  cout << alignof(int) << "\talignof(int)\n";
+  cout << alignof(double) << "\talignof(double)\n";
 }
