31a2ff5b644c6fc433a96fb4c6d8608e693812c3
deanturpin <git@lvbtn.uk>
2018-04-23 12:16:57 +0100

    Uncomment literals

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ all:
 	make $(patsubst %.cpp, %.o, $(wildcard *.cpp))
 
 %.o: %.cpp
-	clang++ -g -Wall -Wextra -pedantic -pedantic-errors -std=c++14 -lpthread -o $@ $<
+	clang++ -g -Wall -Wextra -pedantic -pedantic-errors -std=gnu++14 -lpthread -o $@ $<
 
 wait:
 	while :; do inotifywait -qe modify *.cpp; make; done
--- user_literals.cpp
+++ user_literals.cpp
@@ -3,19 +3,18 @@
 
 int main() {
 
-  using namespace std;
-
-  // cout << R "((?:[0-9]{1,3}\.?){4}\s+\S+)" << endl;
-  // cout << 1'000'000'000 << endl;
-  // cout << hex << 0x0000 '1111' 2222'3333 << dec << endl;
-  // cout << hex << 0b0000011100001111 << dec << endl;
+  std::cout << R"((?:[0-9]{1,3}\.?){4}\s+\S+)" << '\n';
+  std::cout << 1'000'000'000 << '\n';
+  std::cout << std::hex << 0x0000'1111'2222'3333 << std::dec << '\n';
+  std::cout << std::hex << 0b0000011100001111 << std::dec << '\n';
 
   // Time related
+  using namespace std;
   auto time1 = 90s;
   auto time2 = 1min;
   auto time3 = time1 + time2;
 
-  cout << "Total time " << time3.count() << endl;
+  std::cout << "Total time " << time3.count() << '\n';
 
   // template <char...> double operator "" _π(); // OK
 
