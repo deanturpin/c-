37ffb2d233a14fd2d7d7ea58c3909d78ed5a6d01
deanturpin <git@lvbtn.uk>
2018-05-10 08:43:54 +0100

    Add sets test

---
+++ sets.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+
+int main() {
+
+  std::set<std::pair<int, int>> s {
+    {1, 2},
+    {1, 2},
+    {1, 2},
+    {3, 2},
+    {6, 7},
+    {1, 5},
+    {1, 5},
+  };
+
+  std::cout << s.size() << " elements\n";
+  for (const auto &p : s)
+    std::cout << p.first << ' ' << p.second << '\n';
+
+  // for (auto &p : s)
+    // p.first = 0;
+}
