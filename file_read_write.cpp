9b40254713c2ade053db3c225a884aec92a678c6
deanturpin <git@brightcoin.uk>
2018-06-19 22:09:30 +0100

    Declare fstream inline

--- file_read_write.cpp
+++ file_read_write.cpp
@@ -8,19 +8,16 @@ void read_chars() {
 
   // Create container of ascending numbers to write to the file
   const std::vector<char> v{0, 1, 2, 3, 4};
-
-  // File we're reading from and writing to
   const std::string file_name{"ascending.bin"};
 
   // Write
   std::ofstream out(file_name, std::ios::binary);
   std::copy(std::cbegin(v), std::cend(v), std::ostream_iterator<char>(out));
-
   out.close();
 
   // Read
-  std::ifstream in(file_name);
-  const std::vector<char> numbers{std::istream_iterator<char>(in), {}};
+  const std::vector<char> numbers{
+      std::istreambuf_iterator<char>(std::ifstream(file_name).rdbuf()), {}};
 
   std::cout << numbers.size() << " elements\n";
 
@@ -33,15 +30,12 @@ void read_doubles() {
   // Create container of ascending numbers to write to the file
   std::vector<double> v(30);
   std::iota(std::begin(v), std::end(v), 0.0);
-
-  // File we're reading from and writing to
   const std::string file_name{"ascending.txt"};
 
   // Write
   std::ofstream out(file_name);
   std::copy(std::cbegin(v), std::cend(v),
             std::ostream_iterator<int>(out, "\n"));
-
   out.close();
 
   // Read
