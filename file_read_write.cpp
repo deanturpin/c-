4b9a90db7f5f143439d732d19b1eaaabc5ac4954
deanturpin <git@brightcoin.uk>
2018-06-19 18:24:20 +0100

    Add separate files for chars and doubles

--- file_read_write.cpp
+++ file_read_write.cpp
@@ -4,14 +4,38 @@
 #include <numeric>
 #include <vector>
 
-int main() {
+void read_chars() {
+
+  // Create container of ascending numbers to write to the file
+  const std::vector<char> v{0, 1, 2, 3, 4};
+
+  // File we're reading from and writing to
+  const std::string file_name{"ascending.bin"};
+
+  // Write
+  std::ofstream out(file_name, std::ios::binary);
+  std::copy(std::cbegin(v), std::cend(v), std::ostream_iterator<char>(out));
+
+  out.close();
+
+  // Read
+  std::ifstream in(file_name);
+  const std::vector<char> numbers{std::istream_iterator<char>(in), {}};
+
+  std::cout << numbers.size() << " elements\n";
+
+  for (const auto &i : numbers)
+    std::cout << std::to_string(i) << '\n';
+}
+
+void read_doubles() {
 
   // Create container of ascending numbers to write to the file
   std::vector<double> v(30);
   std::iota(std::begin(v), std::end(v), 0.0);
 
   // File we're reading from and writing to
-  const std::string file_name{"numbers.txt"};
+  const std::string file_name{"ascending.txt"};
 
   // Write
   std::ofstream out(file_name);
@@ -30,4 +54,9 @@ int main() {
 
   for (const auto &i : numbers)
     std::cout << i << '\n';
+}
+
+int main() {
+  read_doubles();
+  read_chars();
 }
