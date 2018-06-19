5d523be99d1dab309178ab5ca7183d9c72cde7cd
deanturpin <git@brightcoin.uk>
2018-06-19 17:02:17 +0100

    Read and write with streams

--- init_lists.cpp
+++ init_lists.cpp
@@ -7,21 +7,23 @@
 int main() {
 
   // Create container of ascending numbers to write to the file
-  std::vector<char> v(20);
+  std::vector<int> v(30);
   std::iota(std::begin(v), std::end(v), 0);
 
   // File we're reading from and writing to
-  const std::string file_name{"binary.bin"};
+  const std::string file_name{"random.bin"};
 
   // Write
-  std::copy(std::cbegin(v), std::cend(v),
-            std::ostreambuf_iterator<char>(std::ofstream(file_name).rdbuf()));
+  std::ofstream out(file_name, std::ios::binary);
+  std::copy(std::cbegin(v), std::cend(v), std::ostreambuf_iterator<char>(out));
+
+  out.close();
 
   using word = char;
 
   // Read
-  const std::vector<word> numbers{
-      std::istreambuf_iterator<word>(std::ifstream(file_name).rdbuf()), {}};
+  std::ifstream in(file_name);
+  const std::vector<word> numbers{std::istreambuf_iterator<word>(in), {}};
 
   std::cout << numbers.size() << " elements\n";
 }
