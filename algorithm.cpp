526d20b53a223f991dc9b4fc19dce9853827e6c5
root <root@localhost.localdomain>
2018-07-04 15:56:34 +0100

    Spacing

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,9 +5,7 @@
 int main() {
 
   using namespace std;
-
   const vector<long> vec = {1, 2, 3, 4, 5, 6};
-
   const auto count =
       count_if(vec.cbegin(), vec.cend(), [](const auto &a) { return a < 3; });
 
