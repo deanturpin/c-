863a9034c9d39508284dbcc63da34555c1768bff
deanturpin <dean@brightcoin.uk>
2018-08-06 22:13:08 +0100

    Move to lambda

--- cycles.cpp
+++ cycles.cpp
@@ -11,20 +11,35 @@ int main() {
       {0b00000'00000'00000'00000, 0b0000}, // No bits set
       {0b11111'11111'11111'11111, 0b1111}, // All bits set
       {0b10101'01010'10101'01010, 0b1111}, // Alternate
-      {0b00011'11111'11110'00000, 0b0000}, // Block
+      {0b00011'11111'10000'00000, 0b0110}, // Block
       {0b10001'00000'00000'01000, 0b1100}, // Random
   };
 
-  // Register used by both low- and high-frequency readers
-  bool shared_register = false;
-
   // Iterate over each test pair
   for (const auto & [ input, output ] : test_cases) {
-    std::cout << input << '\n' << output << '\n';
 
+    // Register used by both low- and high-frequency readers
+    bool shared_register = false;
+
+    // Lambda is called less often than shared_register is set
+    const auto low_frequency_reader = [&shared_register]() {
+
+      // TODO
+      // EDIT THIS BIT
+      // TODO
+      const auto value = shared_register;
+      shared_register = false;
+
+      // Return the value to report
+      return value;
+    };
+
+    // Create a container to compare with the expected results from the test
+    // case
     std::bitset<4> results;
 
     // Iterate over each bit in the input sequence
+    std::cout << input << " test\n" << output << " expected\n";
     for (size_t i = 0; i < input.size(); ++i) {
 
       // The high-frequency reader sets the shared register for each input bit
@@ -32,14 +47,12 @@ int main() {
         shared_register = true;
 
       // The low-frequency reader checks the shared register less often
-      if (!(i % 5)) {
-        results.set(i / 5, shared_register);
-        shared_register = false;
-      }
+      if (!(i % 5))
+        results[i / 5] = low_frequency_reader();
     }
 
     // Print results
-    std::cout << results << '\n';
+    std::cout << results << " actual\n";
     std::cout << (results == output ? "PASS" : "FAIL") << "\n\n";
   }
 }
