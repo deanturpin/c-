537b77c2c49f409969b5a3339517689c0aa17744
deanturpin <dean@brightcoin.uk>
2018-08-07 07:54:09 +0100

    Reorder tests

--- cycles.cpp
+++ cycles.cpp
@@ -8,9 +8,9 @@ int main() {
   // A test consists of a pair of input and output bitsets
   using test = std::pair<std::bitset<20>, std::bitset<4>>;
   const std::vector<test> test_cases{
-      {0b00000'00000'00000'00000, 0b0000}, // No bits set
       {0b11111'11111'11111'11111, 0b1111}, // All bits set
-      {0b10101'01010'10101'01010, 0b1111}, // Alternate
+      {0b00000'00000'00000'00000, 0b0000}, // No bits set
+      {0b10101'01010'10101'01010, 0b1110}, // Alternate
       {0b00011'11111'10000'00000, 0b0110}, // Block
       {0b10001'00000'00000'01000, 0b1100}, // Random
   };
@@ -22,9 +22,13 @@ int main() {
     bool shared_register = false;
 
     // The high frequency reader process every state change
-    const auto high_frequency_reader = [&shared_register]() {
-      shared_register = true;
-    };
+    const auto high_frequency_reader =
+        [&shared_register](const bool hw_register) {
+
+          // Latch the shared register high
+          if (hw_register)
+            shared_register = true;
+        };
 
     // The low frequency reader is called less often
     const auto low_frequency_reader = [&shared_register]() {
@@ -41,12 +45,13 @@ int main() {
     std::bitset<4> results;
 
     // Iterate over each bit in the input sequence
-    std::cout << input << " test\n" << output << " expected\n";
+    std::cout << ".    .    .    .    \n"
+              << input << " test\n"
+              << output << " expected\n";
     for (size_t i = 0; i < input.size(); ++i) {
 
       // The high-frequency reader sets the shared register for each input bit
-      if (input.test(i))
-        high_frequency_reader();
+      high_frequency_reader(input.test(i));
 
       // The low-frequency reader checks the shared register less often
       if (!(i % 5))
