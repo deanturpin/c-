57db5b5d63eeb1f86bdc0bac67ab4763abf4fd15
deanturpin <dean@brightcoin.uk>
2018-08-09 22:19:07 +0100

    String version complete

--- cycles.cpp
+++ cycles.cpp
@@ -3,14 +3,6 @@
 #include <string>
 #include <vector>
 
-// {0b11111'11111'11111'11111'11111, 0b01111}, // All bits set
-// {0b00000'00000'00000'00000'00000, 0b00000}, // No bits set
-// {0b10101'01010'10101'01010'00000, 0b01110}, // Alternate
-// {0b00011'11111'00000'00000'00000, 0b01100}, // Block
-// {0b00011'11111'11100'00000'00000, 0b01100}, // Block
-// {0b00000'11111'11111'10000'00000, 0b01100}, // Block
-// {0b10001'00000'00000'01000'00000, 0b01000}, // Random
-
 int main() {
 
   // A test consists of a pair of input and output bitsets, the low frequency
@@ -26,87 +18,41 @@ int main() {
           // 0123|0123|0123|0123|0123|
       };
 
-  // const auto blah = std::make_tuple(0, 1, 2);
-  // const auto rah = std::make_tuple(10, "Test", 3.14);
-
   std::cout << test_cases.size() << " test cases\n";
 
   // Iterate over each test pair
-  for (const auto & [ input, expected, actual ] : test_cases) {
+  for (auto & [ input, expected, actual ] : test_cases) {
 
-    // for (const auto &bit : input)
-    //         std::cout << bit;
-    // std::cout << "\n";
+    const auto high = '-';
+    const auto low = '_';
+
+    bool transition_detected = false;
+    auto previous_state = low;
+
     for (const auto &v : {input, expected, actual})
-      std::cout << std::quoted(v) << '\n';
+      std::cout << v << '\n';
 
-    //     // Register used by both low- and high-frequency readers
-    //     bool event_detected_flag = false;
-    //     bool transition_detected = false;
-    //
-    //     // The low frequency reader is called less often
-    //     const auto low_frequency_reader = [&event_detected_flag,
-    //     &transition_detected]() {
-    //
-    // 	    bool val = false;
-    //
-    // 	    if (transition_detected) {
-    // 		    // std::cout << "transition_detected\n";
-    // 		    transition_detected = false;
-    // 		    val = true;
-    // 	    }
-    // 	    else
-    // 		    val = event_detected_flag;
-    //
-    // 	    // std::cout << val << " val\n";
-    // 	    return val;
-    //     };
-    //
-    //     // The high frequency reader process every state change
-    //     const auto high_frequency_reader =
-    //         [&event_detected_flag, &transition_detected]
-    // 	(const bool event_detected_register) {
-    //
-    // 	// Check if this is a state transition
-    //           if (event_detected_register != event_detected_flag)
-    //           transition_detected = true;
-    //
-    // 	  // Update copy of register
-    // 	event_detected_flag = event_detected_register;
-    //         };
-    //
-    //
-    //     // Create a container to compare with the expected results from the
-    //     test
-    //     // case
-    //     std::bitset<5> results = 0b0000;
-    //     std::stringstream out;
-    //
-    //     // Iterate over each bit in the input sequence
-    //     out << input << " test\n" << output << " expected\n";
-    //     size_t result_index = results.size() - 1;
-    //     for (size_t i = 0; i < input.size(); ++i) {
-    //
-    // 	    // std::cout << i << '\n';
-    //       // The high-frequency reader sets the shared register for each
-    //       input bit
-    //       high_frequency_reader(input.test(i));
-    //
-    //       // The low-frequency reader checks the shared register less often
-    //       if ((i % results.size()) == results.size() - 1) {
-    // 	      const auto r = low_frequency_reader();
-    //
-    // 	      // std::cout << result_index << " becomes " << r << "\n";
-    //         results[result_index--] = r;
-    //       }
-    //
-    //     }
+    size_t low_freq_index = 0;
+    size_t high_freq_index = 0;
 
-    // Print results
-    //     out << results << " actual\n";
-    //     out << (results == output ? "PASS" : "FAIL") << "\n\n";
-    //
-    //     if (results != output)
-    // 	    std::cout << out.str();
+    for (const auto &state : input) {
+
+      // Hight frequency reader records state changes
+      if (state != previous_state) {
+        transition_detected = true;
+        previous_state = state;
+      }
+
+      // Low frequency reader reports state changes and clears flag
+      if (!((high_freq_index + 1) % 5)) {
+        ++low_freq_index;
+        std::cout << (transition_detected ? '-' : previous_state);
+        transition_detected = false;
+      }
+
+      ++high_freq_index;
+    }
+
+    std::cout << " actual\n";
   }
 }
