fb1ea497eb4f2a25f4de1c7bcd7493cfdb1ab647
deanturpin <kali@brightcoin.uk>
2018-09-20 15:35:32 +0100

    Add asserts

--- factorial.cpp
+++ factorial.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <functional>
 #include <iostream>
 
@@ -10,22 +11,34 @@
 // 	  return (... + factorial2(args - 1));
 // 	}
 //
+//
+
+// Template struct
 template <int n> struct factorial3 {
   static const int value = n * factorial3<n - 1>::value;
 };
-
 template <> struct factorial3<0> { static const int value = 1; };
+
+// Template function
+template <int n> int factorial4() { return n * factorial4<n - 1>(); }
+template <> int factorial4<0>() { return 1; }
 
 int main() {
 
-  using func = std::function<unsigned int(unsigned int)>;
-
-  // Recursive factorial function
-  const func factorial1 = [&factorial1](const unsigned int n) {
+  // Recursive lambda
+  using func = const std::function<unsigned int(unsigned int)>;
+  func factorial1 = [&factorial1](const int n) {
     return n == 1 ? 1 : n * factorial1(n - 1);
   };
 
   std::cout << factorial1(5) << " factorial1\n";
-  // std::cout << factorial2(5) << " factorial2\n";
   std::cout << factorial3<5>::value << " factorial3\n";
+  std::cout << factorial4<5>() << " factorial4\n";
+
+  assert(factorial1(5) == 120);
+  assert(factorial3<5>::value == 120);
+  assert(factorial4<5>() == 120);
+
+  // enum { blah = factorial4<5>() };
+  // static_assert(factorial4<5>() == 120);
 }
