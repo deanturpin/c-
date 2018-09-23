80e124623fe6feeb196f0d41a991c91cdca19290
deanturpin <kali@brightcoin.uk>
2018-09-23 16:10:17 +0100

    Add size of classes

---
+++ size_of_classes.cpp
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+int main() {
+
+  class a {
+  } _a;
+  struct b {
+    virtual ~b() {}
+  } _b;
+  struct c : b {
+    virtual ~c() {}
+  } _c;
+
+  assert(sizeof _a == 1);
+  assert(sizeof _b == 8);
+  assert(sizeof _c == 8);
+}
