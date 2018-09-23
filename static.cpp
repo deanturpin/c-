b11da7d10aab9655be2bea314a763cc1a609910a
deanturpin <kali@brightcoin.uk>
2018-09-23 17:23:15 +0100

    Statics

---
+++ static.cpp
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <iomanip>
+#include <string>
+
+struct a {
+  static std::string name;
+  const static int version = 1;
+  static std::string identity() { return name + std::to_string(version); }
+};
+
+std::string a::name = "a";
+
+int main() {
+
+  a _a;
+  assert(_a.identity() == "a1");
+}
