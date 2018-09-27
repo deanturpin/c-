a905cf9c923702cd5e43a1caff719b479f195e0b
deanturpin <kali@brightcoin.uk>
2018-09-27 11:50:33 +0100

    Add lambda example

--- wide_iterators.cpp
+++ wide_iterators.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <numeric>
 #include <vector>
@@ -5,21 +6,46 @@
 int main() {
 
   // Create test vector
-  std::vector<double> a(20);
-  std::iota(a.begin(), a.end(), -10.1);
+  std::vector<double> a(10);
+  std::iota(a.begin(), a.end(), -3.5);
 
   std::cout << "INPUT\n";
   for (const auto &x : a)
     std::cout << x << '\n';
 
-  // Calculate x-point average
-  const size_t filter = 5;
-  std::vector<double> average;
-  for (auto i = a.cbegin(); i < std::prev(a.cend(), filter); ++i)
-    average.push_back(std::accumulate(i, std::next(i, filter), 0.0) /
-                      static_cast<double>(filter));
+  const long int filter = 5;
 
-  std::cout << "AVERAGES\n";
-  for (const auto &x : average)
-    std::cout << x << '\n';
+  // Calculate x-point average
+  {
+    std::vector<double> average;
+    for (auto i = a.cbegin(); i < std::prev(a.cend(), filter); ++i)
+      average.push_back(std::accumulate(i, std::next(i, filter), 0.0) /
+                        static_cast<double>(filter));
+
+    std::cout << "AVERAGES\n";
+    for (const auto &x : average)
+      std::cout << x << '\n';
+  }
+
+  // Calculate x-point average
+  {
+    std::vector<double> average;
+
+    using iter = std::vector<double>::const_iterator;
+    const std::function<void(iter, iter)> av = [&av, &average](iter begin,
+                                                               iter end) {
+      if (std::distance(begin, end) > filter) {
+        average.push_back(
+            std::accumulate(begin, std::next(begin, filter), 0.0) /
+            static_cast<double>(filter));
+        av(std::next(begin), end);
+      }
+    };
+
+    av(a.cbegin(), a.cend());
+
+    std::cout << "AVERAGES\n";
+    for (const auto &x : average)
+      std::cout << x << '\n';
+  }
 }
