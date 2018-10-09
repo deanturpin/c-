ffa0273432e6d846e0f16862ab4b22f683a46861
deanturpin <kali@brightcoin.uk>
2018-10-09 22:15:09 +0100

    Use a using

--- lambda_identity.cpp
+++ lambda_identity.cpp
@@ -1,18 +1,19 @@
 #include <functional>
 
-const std::vector<std::function<int(const int &)>> functions{
-    [](const int &i) { return i * 0; }, [](const int &i) { return i * 1; },
-    [](const int &i) { return i * 2; }, [](const int &i) { return i * 3; },
-    [](const int &i) { return i * 4; }, [](const int &i) { return i * 5; },
-    [](const int &i) { return i * 6; }, [](const int &i) { return i * 7; },
+using param = const int &;
+const std::vector<std::function<int(param)>> functions{
+    [](param i) { return i * 0; }, [](param i) { return i * 1; },
+    [](param i) { return i * 2; }, [](param i) { return i * 3; },
+    [](param i) { return i * 4; }, [](param i) { return i * 5; },
+    [](param i) { return i * 6; }, [](param i) { return i * 7; },
 };
 
 int main() {
 
-  std::vector<int> results;
+  int sum = 0;
   for ([[maybe_unused]] int i = 0; i < 10000; ++i)
     for (const auto &f : functions)
-      results.push_back(f(i));
+      sum += f(i);
 
-  return 0 && results.size();
+  return 0 && sum;
 }
