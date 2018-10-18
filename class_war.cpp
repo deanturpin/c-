b68ee0a542acf689a50055b82f9c52231c541cff
deanturpin <kali@brightcoin.uk>
2018-10-18 18:27:39 +0100

    Simplify into a single dimension array

--- class_war.cpp
+++ class_war.cpp
@@ -5,27 +5,24 @@ unsigned default_constructor_calls = 0;
 unsigned copy_constructor_calls = 0;
 
 struct copy_me {
-  copy_me() { ++default_constructor_calls; }
-  copy_me(const copy_me &) { ++copy_constructor_calls; }
+
+  copy_me() {
+    // TODO - do expensive operation
+    ++default_constructor_calls;
+  }
+
+  copy_me(const copy_me &) {
+    // TODO - do expensive operation
+    ++copy_constructor_calls;
+  }
 };
 
 int main() {
 
-  std::vector<std::vector<copy_me>> x;
-
-  const unsigned width = 4;
-
-  // Create a 2D array of objects
-  for (unsigned i = 0; i < width; ++i) {
-
-    // Populate a new row
-    std::vector<copy_me> y;
-    for (unsigned j = 0; j < width; ++j)
-      y.push_back(copy_me()); // Call to copy constructor
-
-    // Store the row
-    x.push_back(y);
-  }
+  // Lots of calls to default constructor
+  std::vector<copy_me> y;
+  for (unsigned j = 0; j < 17; ++j)
+    y.push_back(copy_me());
 
   std::cout << default_constructor_calls << " default constructor calls\n";
   std::cout << copy_constructor_calls << " copy constructor calls\n";
