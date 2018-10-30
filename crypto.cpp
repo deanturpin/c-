3cfde39708904e542c8a219f99f1bb5f6f89a480
deanturpin <kali@brightcoin.uk>
2018-10-30 15:24:03 +0000

    Add routine to apply encrption lambda

--- crypto.cpp
+++ crypto.cpp
@@ -12,20 +12,6 @@ int main() {
            0b11111111'11111111'11111111'11111110;
   };
 
-  // Encrypt a message by simple rotation
-  const auto encrypt1 = [](const std::string plaintext) {
-    auto ciphertext = plaintext;
-    for (auto &c : ciphertext) {
-      // Encrypt a single character
-      const auto ascii_offset = 32ul;
-      const auto rotate = 1ul;
-      const auto wrap = 126ul - ascii_offset;
-      c = ascii_offset + (c + rotate - ascii_offset) % wrap;
-    }
-
-    return ciphertext;
-  };
-
   // Generate and exchange keys
   const auto a{5ul};
   const auto b{4ul};
@@ -48,21 +34,26 @@ int main() {
                               "r than complex. Complex is better than c"
                               "omplicated. Flat is better than nested. "};
 
-  // Format the message for printing
-  const auto format = [](const std::string in) {
-    unsigned count{1};
-    std::stringstream out;
-    out << "\n\n";
-    for (const auto c : in) {
-      out << c;
-      if (!(count % 40))
-        out << '\n';
+  // Encrypt a message using supplied function
+  const auto encrypt = [](const auto message, const auto func) {
+    // Format the message for printing
+    const auto format = [](const std::string in) {
+      unsigned count{1};
+      std::stringstream out;
+      out << "\n\n";
+      for (const auto c : in) {
+        out << c;
+        if (!(count % 40))
+          out << '\n';
 
-      ++count;
-    }
-    out << "----------------------------------------\n";
+        ++count;
+      }
+      out << "----------------------------------------\n";
 
-    return out.str();
+      return out.str();
+    };
+
+    return format(func(message));
   };
 
   // assert(s1 == s2);
@@ -70,6 +61,23 @@ int main() {
   std::cout << B << " Bill's public key\n";
   std::cout << s1 << " Alice's secret key\n";
   std::cout << s2 << " Bill's secret key\n";
-  std::cout << "plaintext" << format(plaintext);
-  std::cout << "rotate" << format(encrypt1(plaintext));
+
+  // No encryption
+  std::cout << "plaintext" << encrypt(plaintext, [](const std::string message) {
+    return message;
+  });
+
+  // Simple rotation
+  std::cout << "rotate" << encrypt(plaintext, [](const std::string message) {
+    auto ciphertext = message;
+    for (auto &c : ciphertext) {
+      // Encrypt a single character
+      const auto ascii_offset = 32ul;
+      const auto rotate = 1ul;
+      const auto wrap = 126ul - ascii_offset;
+      c = ascii_offset + (c + rotate - ascii_offset) % wrap;
+    }
+
+    return ciphertext;
+  });
 }
