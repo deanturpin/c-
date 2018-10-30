536db44b2cfca5bcc31cc83e22d310857dd4e52c
deanturpin <kali@brightcoin.uk>
2018-10-30 14:45:07 +0000

    Add simple encryption

--- crypto.cpp
+++ crypto.cpp
@@ -1,16 +1,28 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <sstream>
 
 int main() {
 
+  // Create the public key for sharing
   const auto generate_public_key = [](const auto seed,
                                       const auto base) constexpr {
-    return std::lrint(std::pow(base, seed)) % 23;
+    return std::lrint(std::pow(base, seed)) %
+           0b11111111'11111111'11111111'11111110;
   };
 
-  const auto encrypt = [](const std::string plaintext) {
+  // Encrypt a message by simple rotation
+  const auto encrypt1 = [](const std::string plaintext) {
     auto ciphertext = plaintext;
+    for (auto &c : ciphertext) {
+      // Encrypt a single character
+      const auto ascii_offset = 32ul;
+      const auto rotate = 1ul;
+      const auto wrap = 126ul - ascii_offset;
+      c = ascii_offset + (c + rotate - ascii_offset) % wrap;
+    }
+
     return ciphertext;
   };
 
@@ -23,36 +35,41 @@ int main() {
   const auto s2{generate_public_key(B, A)};
 
   // The secret
-  const std::string plaintext{R"(
-llllllllllllllllllllllllllllllllllllllll
-lllllllaaaaaaaaaaaaaaaaaaallllllllllllll
-lllllllaaaaaaaaaaaaaaaaaaallllllllllllll
-llllllllllllllalllllllllllllllllllllllll
-lllllllllllllllallllllllllllllllllllllll
-llllllllllllllllalllllllllllllllllllllll
-lllllllllllllllllallllllllllllllllllllll
-llllllllllllllllllalllllllllllllllllllll
-lllllllllllllllllllallllllllllllllllllll
-llllllllllllllllllllalllllllllllllllllll
-lllllllllllllllllllllallllllllllllllllll
-llllllllllllllllllllllalllllllllllllllll
-lllllllllllllllllllllllallllllllllllllll
-llllllllllllllllllllllllalllllllllllllll
-lllllllllllllllllllllllllallllllllllllll
-llllllllllllllllllllllllllalllllllllllll
-lllllllllllllllllllllllllllallllllllllll
-llllllllllllllllllllllllllllalllllllllll
-llllllllllllllllllllllllllllllllllllllll
-)"};
+  const std::string plaintext{"llllllllllllllllllllllllllllllllllllllll"
+                              "lllllllooooooooooooooooooolllllllllollll"
+                              "lllllllooooooooooooooooooolllllllllollll"
+                              "llllllllllllllollllllllllllllllllllollll"
+                              "lllllllllllllllollllllllollllllllllollll"
+                              "oolllooollllllllllolllllollllllllllollll"
+                              "lloollloollllllllllolllllllllllllllollll"
+                              "                                        "
+                              "Beautiful is better than ugly. Explicit "
+                              "is better than implicit. Simple is bette"
+                              "r than complex. Complex is better than c"
+                              "omplicated. Flat is better than nested. "};
 
-  // Encrypt the secret
-  const auto ciphertext = encrypt(plaintext);
+  // Format the message for printing
+  const auto format = [](const std::string in) {
+    unsigned count{1};
+    std::stringstream out;
+    out << "\n\n";
+    for (const auto c : in) {
+      out << c;
+      if (!(count % 40))
+        out << '\n';
+
+      ++count;
+    }
+    out << "----------------------------------------\n";
+
+    return out.str();
+  };
 
   // assert(s1 == s2);
   std::cout << A << " Alice's public key\n";
   std::cout << B << " Bill's public key\n";
   std::cout << s1 << " Alice's secret key\n";
   std::cout << s2 << " Bill's secret key\n";
-  std::cout << plaintext;
-  std::cout << ciphertext;
+  std::cout << "plaintext" << format(plaintext);
+  std::cout << "rotate" << format(encrypt1(plaintext));
 }
