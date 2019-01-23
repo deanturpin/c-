b0147917f3c4f74ba91366f8c2ffeda06c8c0377
deanturpin <kali@brightcoin.uk>
2019-01-23 12:59:38 +0000

    Add subclass of non virtual

--- size_of_classes.cpp
+++ size_of_classes.cpp
@@ -4,14 +4,20 @@ int main() {
 
   class a {
   } _a;
+
   struct b {
     virtual ~b() {}
   } _b;
+
   struct c : b {
     virtual ~c() {}
   } _c;
 
+  struct d : a {
+  } _d;
+
   assert(sizeof _a == 1);
+  assert(sizeof _d == 1);
   assert(sizeof _b == 8);
   assert(sizeof _c == 8);
 }
