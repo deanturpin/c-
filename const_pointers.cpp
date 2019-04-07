8685e30cd300cc039e8f8d51ece436b3e17f364e
Definition of Dean <defofdean@gmail.com>
2019-04-07 13:17:34 +0100

    Complete const examples

--- const_pointers.cpp
+++ const_pointers.cpp
@@ -2,15 +2,73 @@
 
 int main() {
 
-  // Can change both value and pointer
-  int f  = 4;
-  int h  = 5;
-  int *i = &f;
-  std::cout << *i << '\n';
+  {
+    // Can change both value and pointer
+    int f  = 4;
+    int h  = 5;
+    int *i = &f;
+    std::cout << *i << '\n';
 
-  i = &h;
-  std::cout << *i << '\n';
+    i = &h;
+    std::cout << *i << '\n';
 
-  *i = 6;
-  std::cout << *i << '\n';
+    *i = 6;
+    std::cout << *i << '\n';
+  }
+
+  {
+    // Can change only value
+    int f = 4;
+    // int h  = 5;
+    int *i = &f;
+    std::cout << *i << '\n';
+
+    // i = &h;
+    // std::cout << *i << '\n';
+
+    *i = 6;
+    std::cout << *i << '\n';
+  }
+
+  {
+    // Can change only pointer
+    int f        = 4;
+    int h        = 5;
+    int const *i = &f;
+    // const int * i = &f; // const keyword can also go before the type
+    std::cout << *i << '\n';
+
+    i = &h;
+    std::cout << *i << '\n';
+
+    // *i = 6;
+    // std::cout << *i << '\n';
+  }
+  {
+    // Can change only pointer
+    int f        = 4;
+    int h        = 5;
+    int const *i = &f;
+    std::cout << *i << '\n';
+
+    i = &h;
+    std::cout << *i << '\n';
+
+    // *i = 6;
+    // std::cout << *i << '\n';
+  }
+
+  {
+    // Can change neither value nor pointer
+    int f = 4;
+    // int h  = 5;
+    int const *const i = &f;
+    std::cout << *i << '\n';
+
+    // i = &h;
+    // std::cout << *i << '\n';
+
+    // *i = 6;
+    // std::cout << *i << '\n';
+  }
 }
