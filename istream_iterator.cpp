25b4efc118ede1e00cacad727308481944aff8f3
Definition of Dean <defofdean@gmail.com>
2019-04-27 13:57:28 +0100

    Add istream iterator example

---
+++ istream_iterator.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+
+  std::ifstream in{"pairs.csv"};
+  std::vector<int> vec{std::istream_iterator<int>{in}, {}};
+
+  for (const auto &p : vec)
+    std::cout << p << '\n';
+}
