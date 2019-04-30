a7a4fce06a4a3b73919d7549b59f2ac16ab9414e
Definition of Dean <defofdean@gmail.com>
2019-04-30 08:30:25 +0100

    Trial using comma as delimeter

--- quoted.cpp
+++ quoted.cpp
@@ -6,9 +6,19 @@ int main() {
 
   std::cout << std::quoted("Don't quote me") << '\n';
 
-  std::stringstream in{R"("Hello there" "What time is it?")"};
+  {
+    std::stringstream in{R"("Hello there" "What time is it?")"};
 
-  std::string line;
-  while (in >> std::quoted(line))
-    std::cout << line << '\n';
+    std::string line;
+    while (in >> std::quoted(line))
+      std::cout << line << '\n';
+  }
+
+  {
+    std::stringstream in{"1, 2, 3, 4, 5"};
+
+    std::string line;
+    while (in >> std::quoted(line, ','))
+      std::cout << line << '\n';
+  }
 }
