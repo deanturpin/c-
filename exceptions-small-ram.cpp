27cd51e8c87a7f17ac71a5adafb878667010b1a4
Definition of Dean <defofdean@gmail.com>
2019-05-05 15:48:01 +0000

    Clang format

--- exceptions-small-ram.cpp
+++ exceptions-small-ram.cpp
@@ -3,12 +3,12 @@
 
 int main() try {
 
-	size_t bytes = 35 * 100 * 1000 * 1010ul;
-	std::vector<uint8_t> v(bytes);
+  size_t bytes = 35 * 100 * 1000 * 1010ul;
+  std::vector<uint8_t> v(bytes);
 
-	std::cout << v.size() << " size of container in bytes\n";
-	std::cout << v.max_size() << " max size of container in bytes\n";
+  std::cout << v.size() << " size of container in bytes\n";
+  std::cout << v.max_size() << " max size of container in bytes\n";
 
 } catch (...) {
-	std::cout << "Caught\n";
+  std::cout << "Caught\n";
 }
